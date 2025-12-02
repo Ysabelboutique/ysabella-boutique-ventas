907956a015f93e30cd3570464c29bfcc107a0ebc
Ysabelboutique <ysabellacajamarcaboutique@gmail.com>
2025-12-02 15:55:35 -0500

    Update app.js

--- css/js/app.js
+++ css/js/app.js
@@ -1,2 +1,30 @@
-// Archivo JavaScript
-console.log("Sistema de Boutique listo");
+function agregarVenta() {
+    let producto = document.getElementById("producto").value;
+    let precio = parseFloat(document.getElementById("precio").value);
+    let cantidad = parseInt(document.getElementById("cantidad").value);
+
+    if (!producto || !precio || !cantidad) {
+        alert("Por favor complete todos los campos.");
+        return;
+    }
+
+    let total = precio * cantidad;
+
+    let tabla = document.getElementById("tablaVentas");
+
+    let fila = `
+        <tr>
+            <td>${producto}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>${total}</td>
+        </tr>
+    `;
+
+    tabla.innerHTML += fila;
+
+    // limpia campos
+    document.getElementById("producto").value = "";
+    document.getElementById("precio").value = "";
+    document.getElementById("cantidad").value = "";
+}
